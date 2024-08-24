851190b3a4e8858802d737cd22a928e53678ac55
Hemant Chaudhary <hemuchoudhary231@gmail.com>
2024-08-24 14:50:08 +0530

    Resume Updated Succesfully

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,11 +2,11 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
+import leaf from "../../Assets/Projects/image.png";
 import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
+import editor from "../../Assets/Projects/CampusFood.jpeg";
+import chatify from "../../Assets/Projects/Student_attendance.png";
+import suicide from "../../Assets/Projects/CryptoTracker.jpeg";
 import bitsOfCode from "../../Assets/Projects/blog.png";
 
 function Projects() {
@@ -21,14 +21,59 @@ function Projects() {
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={editor}
+              isBlog={false}
+              title="Campus Food Delivery"
+              description="A web-based platform for food delivery facility within college campus.
+– Developed a food delivery web app, enhancing user experience with an intuitive interface, which increased order
+efficiency by 30%.
+– Integerated optimized routing to achieve reduced delivery time to 20 minutes.
+– Tools & technologies used: NodeJS, MongoDb, HTML, CSS, Javascript, ReactJs, Tailwind CSS, ExpressJs."
+              ghLink="https://github.com/HemantChaudhary1/Campus-food-delivery/tree/main"
+              demoLink="https://campus-food-delivery.vercel.app/"              
+            />
+          </Col>
+
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={chatify}
               isBlog={false}
               title="Student Attendance Tracker"
-              description="Student's can Track their attendance and can bunk classes for fun 😍 build with react.js, Tailwind CSS."
+              description="A web application for tracking student attendance.
+– Architected a student attendance tracker web app, automating attendance calculations and reducing manual errors by
+80%.
+– Facilitated students to track their attendance, ensuring they meet the 75% threshold for class requirements.
+– Tools & technologies used: ReactJs, HTML, CSS, NodeJs, Javascript, Tailwind CSS, MongoDb."
               ghLink="https://github.com/HemantChaudhary1/Student-Attendance-Tracker"
               demoLink=""
+            />
+          </Col>
+
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={leaf}
+              isBlog={false}
+              title="TODO App"
+              description="Is an TODO app in which you can track your daily tasks build frontend using Reactjs and Backend using Nodejs,express,MongoDb."
+              ghLink="https://github.com/HemantChaudhary1/TODO-App"
+              demoLink=""
+            />
+          </Col>
+
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={suicide}
+              isBlog={false}
+              title="Crypto-Tracker"
+              description="A CryptoCurrency-Tracker which tracks your coin Realtime Information build and Gives all details of your coin using Charts using ReactJs, Chart.js, Tailwind CSS ."
+              ghLink="https://github.com/HemantChaudhary1/CryptoCurrency-Tracker"
+              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
             />
           </Col>
 
@@ -42,40 +87,6 @@ function Projects() {
               demoLink="https://hemantchaudhary1.github.io/Tic-Tac-Toe/"
             />
           </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={editor}
-              isBlog={false}
-              title="FoodZilla"
-              description="A FoodDelivery WebApp for college students on which student can order from their favorite restaurant's and Restaurant owner can add their restaurant on the app for better reach to students for more order.It is a full stack application frontend made with Reactjs,Tailwind CSS and Backend using Nodejs,express and MongoDB."
-              ghLink="https://github.com/HemantChaudhary1/Campus-food-delivery/tree/main"
-              demoLink="https://campus-food-delivery.vercel.app/"              
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={leaf}
-              isBlog={false}
-              title="TODO App"
-              description="Is an TODO app in which you can track your daily tasks build frontend using Reactjs and Backend using Nodejs,express,MongoDb."
-              ghLink="https://github.com/HemantChaudhary1/TODO-App"
-              demoLink=""
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={suicide}
-              isBlog={false}
-              title="Crypto-Tracker"
-              description="A CryptoCurrency-Tracker which tracks your coin Realtime Information build and Gives all details of your coin using Charts using ReactJs, Chart.js, Tailwind CSS ."
-              ghLink="https://github.com/HemantChaudhary1/CryptoCurrency-Tracker"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
-            />
-          </Col>
-
         </Row>
       </Container>
     </Container>
